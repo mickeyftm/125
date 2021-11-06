b58e9e0ca61aa6a89665c540720ef78ad086f4d8
polyCENTdev <84585761+mickeyftm@users.noreply.github.com>
2021-11-06 11:40:02 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -60,9 +60,9 @@ const App: React.FC = () => {
             <Route path="/pools">
               <Pools />
             </Route>
-            {/* <Route path="/presale">
+            <Route path="/presale">
               <PreSale />
-            </Route> */}
+            </Route>
             {/* 404 */}
             <Route component={NotFound} />
           </Switch>
