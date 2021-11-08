734276145fef0487f1a54f7afc60a7e2d6002747
AORLAN <asdrubalborrerorojas@gmail.com>
2021-11-08 19:20:11 -0400

    restore1

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -12,7 +12,7 @@ const pools: PoolConfig[] = [
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
-    tokenPerBlock: '0.0574712643678161',
+    tokenPerBlock: '0.07075471698',
 
     sortOrder: 1,
     isFinished: false,
